d343de64b1b3f755b2c789f5bd01b8e84125228b
Julián Serna <julicamnet@gmail.com>
2020-02-05 21:03:51 +0000

    ....

--- blink.js
+++ blink.js
@@ -2,6 +2,6 @@ let five = require('johnny-five');
 let board = new five.Board();
 
 board.on('ready', () =>  {
-    let led = new five.Led(13);
+    let led = new five.Led(8);
     led.blink(1000)
 });
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const GPS = require("gps");
 const Request = require("request-promise");
 import { connect } from "./database";
 
+// Proximity
+const { Board, Proximity, Led } = require('johnny-five');
+const board = new Board(); 
+
 connect();
 
 const port = new SerialPort("/dev/ttyS0", { baudRate: 9600 });
@@ -15,7 +19,7 @@ gps.on("data", data => {
     if (data.quality != null) {
       console.log(" [" + data.lat + ", " + data.lon + "]");
     } else {
-      console.log("no gps fix available");
+      // console.log("no gps fix available");
     }
   }
 });
@@ -27,3 +31,45 @@ parser.on("data", data => {
     throw e;
   }
 });
+
+board.on('ready', () => {
+  const proximity = new Proximity({
+    controller: 'HCSR04',
+    pin: 12
+  });
+  let passangerIn = false;
+  let passangerOut = false;
+  let walking = false;
+
+  proximity.on('change', () => {
+    const { centimeters } = proximity;
+    const ledIn = new Led(8);
+    const ledOut = new Led(13);
+
+    console.log('cm: ', centimeters);
+    if(!walking){
+      if (centimeters > 2 && centimeters <= 10) passangerOut = true
+      else if (centimeters > 15 && centimeters <= 45) passangerIn = true
+      else {
+        passangerIn = false;
+        passangerOut = false;
+      }
+
+      if(passangerIn) {
+        ledIn.on()
+        ledOut.off()
+        walking = true
+        setTimeout(() => walking = false, 5000)
+      }
+      else ledIn.off();
+
+      if(passangerOut) {
+        ledOut.on()
+        ledIn.off()
+        walking = true
+        setTimeout(() => walking = false, 5000)
+      }
+      else ledOut.off()
+    }
+  });
+})
